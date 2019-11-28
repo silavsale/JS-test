5f4a0c4fa37b787800fdc4597be28e1fd15536c0
Sergiy Rudenko <sergiy@powertree.io>
2019-11-28 00:47:42 -0500

    added the few new test's

--- util.test.js
+++ util.test.js
@@ -1,4 +1,4 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
     const text = generateText('Sergo', 29);
@@ -8,4 +8,10 @@ test('should output name and age', () => {
 test('should output data-less text', () => {
     const text = generateText('', null);
     expect(text).toBe(' (null years old)');
+})
+
+test('should generate a valid text output', () => {
+    const text = checkAndGenerate('Sergo', 29);
+    expect(text).toBe('Sergo (29 years old)');
+
 })
